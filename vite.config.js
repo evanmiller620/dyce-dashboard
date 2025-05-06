becad85577f3137619969b5664e7c0c48d7e48d7
evanlmiller620 <evanlmiller20@gmail.com>
2025-05-06 01:17:19 -0400

    add ethers

--- vite.config.js
+++ vite.config.js
@@ -10,9 +10,9 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src')
     }
   },
-  build: {
-    rollupOptions: {
-      external: ['ethers']
-    }
-  }
+  // build: {
+  //   rollupOptions: {
+  //     external: ['ethers']
+  //   }
+  // }
 })
